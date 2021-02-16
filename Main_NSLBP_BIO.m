3244cc591c0517586eb2384ed8edc040441dc3ca
Florent Moissenet <florent.moissenet@protonmail.com>
2021-02-16 08:44:01 +0100

    Add computation of biomarkers

--- Main_NSLBP_BIO.m
+++ Main_NSLBP_BIO.m
@@ -364,7 +364,7 @@ end
 % central biomarker, dim 2 if right/left biomarker)
 
 % BMo3
-% Sit to stand	Pelvis/leg	Spatial/intensity	Hip sagittal angle (rom)
+% d410 "Changing basic body position" Sit to stand	Pelvis/leg	Spatial/intensity	Hip sagittal angle (rom)
 disp('  - BMo3');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -390,7 +390,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo4
-% Stand to sit	Pelvis/leg	Spatial/intensity	Hip sagittal angle (rom)
+% d410 "Changing basic body position" Stand to sit	Pelvis/leg	Spatial/intensity	Hip sagittal angle (rom)
 disp('  - BMo4');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -416,13 +416,21 @@ for i = 1:size(Trial,2)
 end
 
 % BMo5
-% Trunk sagittal bending	Lumbar	Spatial/intensity	Lower lumbar sagittal angle (max)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Lower lumbar sagittal angle (max)
 disp('  - BMo5');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
         temp = [];
-        for icycle = 1:size(Trial(i).Joint(14).Euler.rcycle,4)
-            temp = [temp max(Trial(i).Joint(14).Euler.rcycle(:,:,1,icycle),[],1)];
+        for icycle = 1:size(Trial(i).Segment(15).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(15).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(15).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            end
         end
         Biomarker.BMo5.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
         Biomarker.BMo5.units                                 = '°deg';
@@ -431,28 +439,44 @@ for i = 1:size(Trial,2)
 end
 
 % BMo6
-% Trunk sagittal bending	Lumbar	Spatial/intensity	Lower lumbar sagittal angular velocity (max)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Lower lumbar sagittal angular velocity (max)
 disp('  - BMo6');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
         temp = [];
-        for icycle = 1:size(Trial(i).Joint(14).Euler.rcycle,4)
-            temp = [temp max(gradient(Trial(i).Joint(14).Euler.rcycle(:,:,1,icycle)),[],1)];
+        for icycle = 1:size(Trial(i).Segment(15).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(15).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(gradient(value),[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(15).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(gradient(value),[],1)];
+                clear value;
+            end
         end
         Biomarker.BMo6.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
-        Biomarker.BMo6.units                                 = '°deg/frame';
+        Biomarker.BMo6.units                                 = '°deg.frame-1';
         clear temp;
     end
 end
 
 % BMo9
-% Trunk sagittal bending	Thorax	Spatial/intensity	Lower thorax sagittal angle (max)
+% d410 "Changing basic body position" Trunk sagittal bending	Thorax	Spatial/intensity	Lower thorax sagittal angle (max)
 disp('  - BMo9');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
         temp = [];
-        for icycle = 1:size(Trial(i).Joint(16).Euler.rcycle,4)
-            temp = [temp max(Trial(i).Joint(16).Euler.rcycle(:,:,1,icycle),[],1)];
+        for icycle = 1:size(Trial(i).Segment(17).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(17).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(17).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            end
         end
         Biomarker.BMo9.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
         Biomarker.BMo9.units                                 = '°deg';
@@ -461,7 +485,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo10
-% Trunk sagittal bending	Lumbar	Spatial/intensity	Lumbar contribution to thorax angle (rom)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Lumbar contribution to thorax angle (rom)
 disp('  - BMo10');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
@@ -491,13 +515,21 @@ for i = 1:size(Trial,2)
 end
 
 % BMo12
-% Trunk sagittal bending	Lumbar	Spatial/intensity	Lumbar sagittal angle (max)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Lumbar sagittal angle (max)
 disp('  - BMo12');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
         temp = [];
-        for icycle = 1:size(Trial(i).Joint(10).Euler.rcycle,4)
-            temp = [temp max(Trial(i).Joint(10).Euler.rcycle(:,:,1,icycle),[],1)];
+        for icycle = 1:size(Trial(i).Segment(11).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(11).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(11).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            end
         end
         Biomarker.BMo12.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
         Biomarker.BMo12.units                                 = '°deg';
@@ -506,22 +538,30 @@ for i = 1:size(Trial,2)
 end
 
 % BMo17
-% Trunk sagittal bending	Lumbar	Spatial/intensity	Lumbar sagittal angular velocity (max)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Lumbar sagittal angular velocity (max)
 disp('  - BMo17');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
         temp = [];
-        for icycle = 1:size(Trial(i).Joint(10).Euler.rcycle,4)
-            temp = [temp max(gradient(Trial(i).Joint(10).Euler.rcycle(:,:,1,icycle)),[],1)];
+        for icycle = 1:size(Trial(i).Segment(11).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(11).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(gradient(value),[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(11).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(gradient(value),[],1)];
+                clear value;
+            end
         end
         Biomarker.BMo17.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
-        Biomarker.BMo17.units                                 = '°deg/frame';
+        Biomarker.BMo17.units                                 = '°deg.frame-1';
         clear temp;
     end
 end
 
 % BMo23
-% Sit to stand	Lumbar/leg	Spatial/intensity	Lumbar/hip ratio of sagittal angle (rom)
+% d410 "Changing basic body position" Sit to stand	Lumbar/leg	Spatial/intensity	Lumbar/hip ratio of sagittal angle (rom)
 disp('  - BMo23');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -544,7 +584,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo24
-% Stand to sit	Lumbar/leg	Spatial/intensity	Lumbar/hip ratio of sagittal angle (rom)
+% d410 "Changing basic body position" Stand to sit	Lumbar/leg	Spatial/intensity	Lumbar/hip ratio of sagittal angle (rom)
 disp('  - BMo24');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -567,7 +607,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo25
-% Sit to stand	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (max)
+% d410 "Changing basic body position" Sit to stand	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (max)
 disp('  - BMo25');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -589,7 +629,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo26
-% Sit to stand	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (mean)
+% d410 "Changing basic body position" Sit to stand	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (mean)
 disp('  - BMo26');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -611,7 +651,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo27
-% Stand to sit	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (mean)
+% d410 "Changing basic body position" Stand to sit	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (mean)
 disp('  - BMo27');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -633,7 +673,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo28
-% Sit to stand	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (min)
+% d410 "Changing basic body position" Sit to stand	Lumbar/leg	Coordination	Lumbar/hip relative phase difference (min)
 disp('  - BMo28');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -655,7 +695,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo29
-% Trunk sagittal bending	Lumbar/pelvis	Coordination	Lumbar/pelvis absolute relative phase (mean)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar/pelvis	Coordination	Lumbar/pelvis absolute relative phase (mean)
 disp('  - BMo29');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
@@ -695,7 +735,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo30
-% Trunk sagittal bending	Lumbar/pelvis	Coordination	Lumbar/pelvis deviation phase (mean)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar/pelvis	Coordination	Lumbar/pelvis deviation phase (mean)
 disp('  - BMo30');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
@@ -735,7 +775,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo33
-% Sit to stand	Lumbar/pelvis	Spatial/intensity	Lumbopelvic sagittal angle (rom)
+% d410 "Changing basic body position" Sit to stand	Lumbar/pelvis	Spatial/intensity	Lumbopelvic sagittal angle (rom)
 disp('  - BMo33');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -751,7 +791,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo34
-% Stand to sit	Lumbar/pelvis	Spatial/intensity	Lumbopelvic sagittal angle (rom)
+% d410 "Changing basic body position" Stand to sit	Lumbar/pelvis	Spatial/intensity	Lumbopelvic sagittal angle (rom)
 disp('  - BMo34');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'S2S_Unconstrained')
@@ -767,7 +807,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo37
-% Trunk sagittal bending	Pelvis	Spatial/intensity	Pelvis sagittal angle (max)
+% d410 "Changing basic body position" Trunk sagittal bending	Pelvis	Spatial/intensity	Pelvis sagittal angle (max)
 disp('  - BMo37');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
@@ -790,7 +830,7 @@ for i = 1:size(Trial,2)
 end
 
 % BMo42
-% Trunk sagittal bending	Lumbar/pelvis	Coordination	Lumbar/pelvis deviation phase (mean)
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar/pelvis	Coordination	Lumbar/pelvis deviation phase (mean)
 disp('  - BMo42');
 for i = 1:size(Trial,2)
     if contains(Trial(i).type,'Trunk_Forward')
@@ -840,5 +880,153 @@ for i = 1:size(Trial,2)
         Biomarker.BMo42.value(igroup,iparticipant,isession,2) = rad2deg(DP2);
         Biomarker.BMo42.units                                 = '°deg';
         clear ang1 nang1 vel1 nvel1 phi1 ang2 nang2 vel2 nvel2 phi2 ang3 nang3 vel3 nvel3 phi3 relativePhase1 relativePhase2 DP1 DP2;  
+    end
+end
+
+% BMo43
+% d410 "Changing basic body position" Trunk rotation	Thorax	Spatial/intensity	Scapular belt transversal angle (max)
+disp('  - BMo43');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Trunk_Rotation')
+        temp = [];
+        for icycle = 1:size(Trial(i).Marker(54).Trajectory.rcycle,3)
+            vec1 = [];
+            vec2 = [];
+            ang  = [];
+            vec1 = Trial(i).Marker(54).Trajectory.rcycle(:,1:2,icycle) - Trial(i).Marker(59).Trajectory.rcycle(:,1:2,icycle);
+            vec1 = [vec1 zeros(101,1)];
+            vec2 = Trial(i).Marker(1).Trajectory.rcycle(:,1:2,icycle) - Trial(i).Marker(6).Trajectory.rcycle(:,1:2,icycle);
+            vec2 = [vec2 zeros(101,1)];
+            for t = 1:101
+                ang(t)  = atan2(norm(cross(vec1(t,:),vec2(t,:))),dot(vec1(t,:),vec2(t,:)));
+            end
+            temp = [temp max(ang)];
+        end
+        Biomarker.BMo43.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo43.units                                 = '°deg';
+        clear temp vec1 vec2 ang;
+    end
+end
+
+% BMo44
+% d410 "Changing basic body position" Trunk sagittal bending	Thorax/pelvis	Spatial/intensity	Thoracopelvic sagittal angle (max)
+disp('  - BMo44');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Trunk_Forward')
+        temp = [];
+        for icycle = 1:size(Trial(i).Joint(10).Euler.lcycle,4)
+            temp = [temp max(abs(Trial(i).Joint(10).Euler.rcycle(:,:,1,icycle)) + ...
+                             abs(Trial(i).Joint(11).Euler.rcycle(:,:,1,icycle)))];
+        end
+        Biomarker.BMo44.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo44.units                                 = '°deg';
+        clear temp;
+    end
+end
+
+% BMo49
+% d410 "Changing basic body position" Trunk sagittal bending	Thorax	Spatial/intensity	Thorax sagittal angle (rom)
+disp('  - BMo49');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Trunk_Forward')
+        temp = [];
+        for icycle = 1:size(Trial(i).Segment(12).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(12).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(value,[],1)-min(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(12).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(value,[],1)-min(value,[],1)];
+                clear value;
+            end
+        end
+        Biomarker.BMo49.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo49.units                                 = '°deg';
+        clear temp;
+    end
+end
+
+% BMo57
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Upper lumbar sagittal angle (max)
+disp('  - BMo57');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Trunk_Forward')
+        temp = [];
+        for icycle = 1:size(Trial(i).Segment(16).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(16).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(16).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            end
+        end
+        Biomarker.BMo57.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo57.units                                 = '°deg';
+        clear temp;
+    end
+end
+
+% BMo58
+% d410 "Changing basic body position" Trunk sagittal bending	Lumbar	Spatial/intensity	Upper lumbar sagittal angular velocity (max)
+disp('  - BMo58');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Trunk_Forward')
+        temp = [];
+        for icycle = 1:size(Trial(i).Segment(16).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(16).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(gradient(value),[],1)-min(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(16).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(gradient(value),[],1)-min(value,[],1)];
+                clear value;
+            end
+        end
+        Biomarker.BMo58.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo58.units                                 = '°deg.frame-1';
+        clear temp;
+    end
+end
+
+% BMo59
+% d410 "Changing basic body position" Trunk sagittal bending	Thorax	Spatial/intensity	Upper thorax sagittal angle (max)
+disp('  - BMo59');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Trunk_Forward')
+        temp = [];
+        for icycle = 1:size(Trial(i).Segment(18).Euler.rcycle,4)
+            if abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,1),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,1),1))>0.1 % Y forward
+                value = pi/2-Trial(i).Segment(18).Euler.rcycle(:,1,2,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            elseif abs(mean(Trial(i).Marker(1).Trajectory.smooth(:,2),1)-mean(Trial(i).Marker(6).Trajectory.smooth(:,2),1))>0.1 % X forward
+                value = pi/2-Trial(i).Segment(18).Euler.rcycle(:,1,1,icycle);
+                temp  = [temp max(value,[],1)];
+                clear value;
+            end
+        end
+        Biomarker.BMo59.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo59.units                                 = '°deg';
+        clear temp;
+    end
+end
+
+% BMo80
+% d430 "Lifting and carrying objects" Trunk sagittal bending	Lumbar/pelvis	Spatial/intensity	Lumbopelvic sagittal angle (max)
+disp('  - BMo80');
+for i = 1:size(Trial,2)
+    if contains(Trial(i).type,'Weight_Unconstrained')
+        temp = [];
+        for icycle = 1:size(Trial(i).Joint(10).Euler.rcycle,4)
+            temp = [temp max(Trial(i).Joint(10).Euler.rcycle(:,:,1,icycle),[],1)];
+        end
+        Biomarker.BMo80.value(igroup,iparticipant,isession,1) = rad2deg(mean(temp));
+        Biomarker.BMo80.units                                 = '°deg';
+        clear temp;
     end
 end
